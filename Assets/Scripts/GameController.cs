79d0d775daa425ac306ea0e3987c59edd6e2e1b8
Michael Yoffe <myoffe@gmail.com>
2015-03-10 00:03:22 +0200

    spawning waves

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,13 +5,26 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start() {
-		SpawnWaves();
+		StartCoroutine(SpawnWaves());
 	}
 
-	void SpawnWaves() {
-		var spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Instantiate(hazard, spawnPosition, Quaternion.identity);
+	IEnumerator SpawnWaves() {
+		yield return new WaitForSeconds(startWait);
+
+		while(true) {
+			for (int i=0; i<hazardCount; i++) {
+				var spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Instantiate(hazard, spawnPosition, Quaternion.identity);
+				yield return new WaitForSeconds(spawnWait);
+			}
+
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }
